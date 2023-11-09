068140dc18ffbd9a8dbdc29322cb38beb7beb253
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-11-09 14:01:31 +0530

    Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const accessKey = "otJ4mIcrBLHmDd1NIMIW0UPhWAEDZULWHAXX-K9UTX4"
+
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("search-input")
+const searchResults = document.querySelector(".search-results")
+const showMore = document.getElementById("")
